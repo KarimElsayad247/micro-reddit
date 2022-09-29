829db8e98ea283bb75d61026f809fec6f0086e54
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-09-29 18:22:49 +0200

    Fix typo causing syntax error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true
-  validates :user_id, presense: true
-  validates :post_id, presense: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
